abc38a134e5f9fff175edf7cc0c713844493d009
Sarhad Bahrami <Sarhad94127@hotmail.com>
2024-03-28 00:41:40 +0100

    styling changes

--- Webshop/sum-script.js
+++ Webshop/sum-script.js
@@ -1,12 +1,10 @@
 window.onload = function () {
-    const person = JSON.parse(localStorage.getItem("person"));
-    console.log(person);
-    //localStorage.getItem("namn")
-    document.getElementById('namn').innerText = person.firstname + " " + person.surname;
-    document.getElementById('email').innerText = person.email;
-    document.getElementById('telefon').innerText = person.phonenumber;
-    document.getElementById('adress').innerText = person.streetaddress;
-    document.getElementById('adress2').innerText = person.zipcode + " " + person.location;
-    //document.getElementById('produkt').innerText = person.;
+    const orderInfo = JSON.parse(localStorage.getItem("person"));
+    const productTitle = localStorage.getItem("prodTitle");
 
+    document.getElementById('namn').innerText = orderInfo.firstname + " " + orderInfo.surname;
+    document.getElementById('email').innerText = orderInfo.email;
+    document.getElementById('telefon').innerText = orderInfo.phonenumber;
+    document.getElementById('adress').innerText = orderInfo.streetaddress;
+    document.getElementById('produkt').innerText = productTitle;
 }
