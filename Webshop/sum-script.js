254c23fa95907572a5f48ac47620d2de88a6ed7e
Sarhad Bahrami <Sarhad94127@hotmail.com>
2024-03-28 09:14:49 +0100

    Styling changes orderinfo

--- Webshop/sum-script.js
+++ Webshop/sum-script.js
@@ -1,10 +1,14 @@
 window.onload = function () {
     const orderInfo = JSON.parse(localStorage.getItem("person"));
     const productTitle = localStorage.getItem("prodTitle");
+    const productPrice = localStorage.getItem("Price"); 
+    const exchangeRate = 8.50;
+    const productPriceSEK = productPrice * exchangeRate;
 
     document.getElementById('namn').innerText = orderInfo.firstname + " " + orderInfo.surname;
     document.getElementById('email').innerText = orderInfo.email;
     document.getElementById('telefon').innerText = orderInfo.phonenumber;
     document.getElementById('adress').innerText = orderInfo.streetaddress;
     document.getElementById('produkt').innerText = productTitle;
+    document.getElementById('pris').innerText = `${productPriceSEK.toFixed(2)} SEK`; 
 }
